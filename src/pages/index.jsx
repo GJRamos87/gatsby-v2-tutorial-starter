25d08594b4102e8d2d3e4d1ec0ee1d7e223d8395
George R <GJRamos87@gmail.com>
2019-07-25 14:31:22 -0400

    Update index.jsx
    Test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ const Index = ({ data }) => {
   return (
     <Layout>
       <Helmet title={'Home Page'} />
-      <Header title="Home Page">Gatsby Tutorial Starter</Header>
+      <Header title="Home Page">Welcome to Georges Thing</Header>
       <PostWrapper>
         {edges.map(({ node }) => (
           <PostList
